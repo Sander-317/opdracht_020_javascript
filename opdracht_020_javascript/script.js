bf98005231298525d1cddc6942f44be9ce62bf55
Sander-317 <80258328+Sander-317@users.noreply.github.com>
2021-04-08 14:19:58 +0200

    Add files via upload

---
+++ opdracht_020_javascript/script.js
@@ -0,0 +1,8 @@
+console.log("Hallo Winc Academy studenten")
+// dit is een grote som
+// een comment word genegeerd als je de code runt 
+/*
+console.log(1230941 * 1823794)
+
+console.log(637263 / 54)
+*/
